a0425a1c5a13ab43f5d7b1897f20fdd621ad18bc
Juan Zinser <juanzinser@gmail.com>
2018-01-14 22:39:09 -0600

    adds plots with maybe case

--- postcursos/maybe_roc.py
+++ postcursos/maybe_roc.py
@@ -1,18 +1,81 @@
 import pandas as pn
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import re
+import numpy as np
 
 
-data = pn.read_csv("~/Workspace/Tesis/data/census/maybe/roc/df.csv")
-df = data[data.columns[1:]]
+datar = pn.read_csv("~/Workspace/Tesis/data/census/maybe/roc/df.csv")
+df = datar[datar.columns[1:]]
 
 df["privacy"] = df["case"].map(lambda x: re.findall("\d+", x)[0])
 df["real"] = df["case"].map(lambda x: re.findall("[^\d]",x)[0])
 df["uniform"] = df["case"].map(lambda x: int(re.findall("[^\d]",x)[1] == "t"))
 
 
+# make one roc curve for each case, also get the roc of the hierarchical levels
+def rocs_by_privacy(df, real, uniform):
+    #real = "t"
+    #uniform = 0
 
-# make one roc curve for each case
+    data = df
+    fig, ax = plt.subplots()
+    labels = []
 
-fig, ax = plt.subplots()
+    for pr in data.privacy.unique():
+        data_f = data.query("privacy == '{pr}'".format(pr=int(pr))) if pr is not None else data
+        data_f = data_f.query("real=='{real}'".format(real=real)) if real is not None else data_f
+        data_f = data_f.query("uniform=={uniform}".format(uniform=uniform)) if uniform is not None else data_f
+
+        x = data_f.fpr
+        y = data_f.tpr
+        lines, _ = ax.get_legend_handles_labels()
+        ax.plot(x, y)
+        labels.append(pr)
+
+    ax.legend(lines, labels, loc='best')
+    tt = "Income DB, real=" + str(real) + ", uniform=" + str(uniform)
+    tt_save = "privacy" + str(real) + str(uniform)
+    ax.set_title(tt)
+    ax.set_xlabel("privacy")
+    plt.savefig("plots/income_roc_" + tt_save + ".png")
+    plt.show()
+
+def rocs_gb(df):
+    #real = "t"
+    #uniform = 0
+
+    data = df
+    fig, ax = plt.subplots()
+    labels = []
+
+    for real in ["t", "m", "f"]:
+        for uniform in [0, 1]:
+    #for pr in data.privacy.unique():
+            #data_f = data.query("privacy == '{pr}'".format(pr=int(pr))) if pr is not None else data
+            data_f = data.query("real=='{real}'".format(real=real)) if real is not None else data
+            data_f = data_f.query("uniform=={uniform}".format(uniform=uniform)) if uniform is not None else data_f
+            data_f.loc[:,"fpr_dis"] = data_f["fpr"].map(lambda x: round(x,2))
+
+            gb = data_f.groupby("fpr_dis")["tpr"].mean().reset_index().sort_values(by="fpr_dis", ascending=True)
+            x = gb.fpr_dis
+            y = pn.rolling_mean(gb.tpr,3)
+            lines, _ = ax.get_legend_handles_labels()
+            ax.plot(x, y)
+            tt = "real=" + str(real) + ", uniform=" + str(uniform)
+
+            labels.append(tt)
+
+    ax.legend(lines, labels, loc='best')
+    tt = "ROC considering if real and distribution"
+    tt_save = "privacy_grouped"
+    ax.set_title(tt)
+    ax.set_xlabel("privacy")
+    plt.savefig("plots/income_roc_" + tt_save + ".png")
+    plt.show()
+
+rocs_gb(df)
+
+for real in ["t", "m", "f"]:
+    for uniform in [0, 1]:
+        rocs_by_privacy(df, real, uniform)
 
